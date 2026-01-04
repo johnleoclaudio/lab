ba9f99ce0f4b81368964eceb5b10d3f61e03d7ee
Leo Claudio <11918571+zllec@users.noreply.github.com>
2026-01-04 12:03:47 +0800

    feat: solved ch1 l5

--- experiments/go/httpgo/main.go
+++ experiments/go/httpgo/main.go
@@ -1,10 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	strChan := make(chan string)
+
+	var a = make([]byte, 8)
+	var line string
+
+	go func() {
+		for {
+			isEOF, err := f.Read(a)
+			if err != nil {
+				log.Println(err)
+			}
+
+			if isEOF == 0 {
+				close(strChan)
+				return
+			}
+
+			stringedA := string(a)
+
+			if strings.Contains(stringedA, "\n") {
+				str := strings.Split(stringedA, "\n")
+				line = line + strings.Join(str[:1], "")
+				strChan <- line
+
+				line = ""
+				line = line + strings.Join(str[1:], "")
+				continue
+			}
+
+			line = line + string(a)
+		}
+	}()
+
+	return strChan
+}
 
 func main() {
 	file, err := os.Open("message.txt")
@@ -13,31 +52,7 @@ func main() {
 		return
 	}
 
-	var a = make([]byte, 8)
-	var line string
-
-	for {
-		isEOF, err := file.Read(a)
-		if err != nil {
-
-			log.Println(err)
-			return
-		}
-
-		if isEOF == 0 {
-			return
-		}
-
-		if strings.Contains(string(a), "\n") {
-			str := strings.Split(string(a), "\n")
-			line = line + strings.Join(str[:1], "")
-			log.Println("read:", line)
-
-			line = ""
-			line = line + strings.Join(str[1:], "")
-			continue
-		}
-
-		line = line + string(a)
+	for s := range getLinesChannel(file) {
+		fmt.Println("read:", s)
 	}
 }
